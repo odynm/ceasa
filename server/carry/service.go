0997d5e3d87476a1c13b0343a4940537270ddc07
Mathias Luiz Ody <mathias.ody@dbccompany.com.br>
2020-11-24 23:18:10 -0300

    .

--- server/carry/service.go
+++ server/carry/service.go
@@ -3,6 +3,7 @@ package carry
 import (
 	"net/http"
 
+	orderPkg "ceasa/order"
 	"ceasa/storage"
 	"ceasa/utils"
 )
@@ -20,12 +21,15 @@ func FinishCarrying(order OrderCarryFinish, userId int, loaderId int, w http.Res
 	response := DbFinishCarrying(order.OrderId, userId, loaderId)
 
 	// Put not delivered items back on storage
-	// In future we will store those rejected items to consultation
+	// In future we will store those rejected items for consultation
 	if order.Products != nil {
 		for _, product := range order.Products {
 			storageId, ok := DbGetStorageIdFromProductOrderId(userId, product.Id)
 			if ok {
 				storage.DbIncreaseAmount(storageId, product.Amount-product.AmountDelivered, userId)
+				// So that we don't have problems in the balances, we gonna take the item away from order
+				// If we want to know the not delivered items, we need a different table to keep track of that
+				orderPkg.DbDecreaseAmountOfProduct(order.OrderId, storageId, product.Amount-product.AmountDelivered, userId)
 			}
 		}
 	}
