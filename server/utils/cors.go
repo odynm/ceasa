07f74fc1cdf6e752e21a908e96459453cfa5a35e
Mathias Ody <mathias.ody@dbccompany.com.br>
2021-04-19 21:45:48 -0300

    testes no cors

--- server/utils/cors.go
+++ server/utils/cors.go
@@ -1,13 +1,15 @@
 package utils
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
 
 func AllowCors(w http.ResponseWriter, r *http.Request) {
 	origin := r.Header.Get("Origin")
-	if strings.HasPrefix(origin, "https://ceasa-bo.web.app/") {
+	fmt.Println("ASDFASDFASDFASDF DADO:" + origin)
+	if strings.HasPrefix(origin, "https://ceasa-bo.web.app") {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, Auth, Timezone")
