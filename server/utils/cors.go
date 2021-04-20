7a255b6e3876f3dc385710eb23d3be2390c009f9
Mathias Ody <mathias.ody@dbccompany.com.br>
2021-04-19 21:39:33 -0300

    alterar cors

--- server/utils/cors.go
+++ server/utils/cors.go
@@ -1,10 +1,13 @@
 package utils
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func AllowCors(w http.ResponseWriter, r *http.Request) {
-	if origin := r.Header.Get("Origin"); origin != "" {
-		// TODO I think it should allow only my origin
+	origin := r.Header.Get("Origin")
+	if strings.HasPrefix(origin, "https://ceasa-bo.web.app/") {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, Auth, Timezone")
