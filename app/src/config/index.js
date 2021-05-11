919840853673a69f17b1f7156f43db93a2f1cafb
Mathias Ody <mathias.ody@dbccompany.com.br>
2021-05-10 22:04:17 -0300

    version 210510

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {    // API_URL: 'http://192.168.100.7:10000',    API_URL: 'https://ceasa-estoque.herokuapp.com',
     REFRESH_RATE_MS: 5000,
-    VERSION: 'Beta 21.05.08',
+    VERSION: 'Beta 21.05.10',
 }
 
 export default config
